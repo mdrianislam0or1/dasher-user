42f2ca780954a9141d63b3cdc21ac295213212d3
Alejandro Salcido <alejandrosalciete77@gmail.com>
2021-10-14 03:57:27 -0600

    fix: main page re-added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-// import { Text } from '../components'
-// import deliveryMan from '../assets/delivery-man.png'
+import { Text } from '../components'
+import deliveryMan from '../assets/delivery-man.png'
 import Form from '../components/Form'
 
 const Home = () => (
   <div>
-    {/* <Text as="h1" color="secondary" large>
+    <Text as="h1" color="secondary" large>
       Hola!
     </Text>
     <Text small>Revisemos como viene tu envío</Text>
     <img src={deliveryMan} alt="Delivery man" />
     <Text as="button" bold uppercase>
       Aceptar
-    </Text> */}
+    </Text>
     <Form />
   </div>
 )
